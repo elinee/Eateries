76593c86a1ddc95ef1e42f5d738833e673fb9665
elinee <>
2018-06-05 13:44:07 +0300

    unit 11
    Add images with circle layer(mask)

--- Eateries/EateriesTableViewController.swift
+++ Eateries/EateriesTableViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class EateriesTableViewController: UITableViewController {
 
+    let restaurantNames = ["Ogonёk Grill&Bar", "Елу", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Respublica", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    let restaurantImages = ["ogonek.jpg", "elu.jpg", "bonsai.jpg", "dastarhan.jpg", "indokitay.jpg", "x.o.jpg", "balkan.jpg", "respublika.jpg", "speakeasy.jpg", "morris.jpg", "istorii.jpg", "klassik.jpg", "love.jpg", "shok.jpg", "bochka.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,23 +25,26 @@ class EateriesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return restaurantNames.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! EateriesTableViewCell
+        
+        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.thumbnailImageView.layer.cornerRadius = 32.5
+        cell.thumbnailImageView.clipsToBounds = true // crop the Imagy by mask (layer)
+        cell.nameLabel.text = restaurantNames[indexPath.row]
+        
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
